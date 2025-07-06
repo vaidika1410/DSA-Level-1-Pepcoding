e314315cf42a71d702d5da5441d0bdc1319d7ceb
Vaidika Kaul <vaidikakaul20@gmail.com>
2025-07-06 15:30:29 +0530

    Comparison of two numbers

---
+++ ComparisonOfTwoNumbers.java
@@ -0,0 +1,14 @@
+public class ComparisonOfTwoNumbers {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 7;
+
+        if(a == b){
+            System.out.println(a + " and " + b + " are equal");
+        } else if(a > b){
+            System.out.println(a + " is greater than " + b);
+        } else {
+            System.out.println(a + " is smaller than " + b);
+        }
+    }
+}
